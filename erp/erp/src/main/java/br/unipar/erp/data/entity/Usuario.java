4ca35ea15210f5e9f1dfd1e6a8efb512eb1c4e28
Zimmermann51 <160682535+Zimmermann51@users.noreply.github.com>
2024-05-27 22:32:59 -0300

    Add files via upload

--- erp/erp/src/main/java/br/unipar/erp/data/entity/Usuario.java
+++ erp/erp/src/main/java/br/unipar/erp/data/entity/Usuario.java
@@ -1,4 +1,16 @@
 package br.unipar.erp.data.entity;
 
+import java.time.LocalDate;
+
 public class Usuario {
+
+    private Integer codigo;
+
+    private String usuario;
+
+    private String senha;
+
+    private LocalDate dataNascimento;
+
+    
 }
